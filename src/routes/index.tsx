52d5c9b60ad3d9ac411b2eb8ddc8662b3fe4bf38
nyun-nye <2001_11_7@naver.com>
2025-10-19 23:48:11 +0900

    ✨: 랜딩페이지 생성

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
 import { Test } from "../pages/Test";
+import { LandingPage } from "../pages/LandingPage";
 
 export const router = createBrowserRouter([
   {
     path: "/test",
     element: <Test />,
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+    ],
   },
   // 향후 페이지 추가 시 아래 형식에 맞춰서 사용
   // {
