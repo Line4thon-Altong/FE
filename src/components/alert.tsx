dbc95330a866f201979f56f78b3086cee97c8e06
nyun-nye <2001_11_7@naver.com>
2025-10-20 01:50:02 +0900

    ✨: alert 모달 구현

---
+++ src/components/alert.tsx
@@ -0,0 +1,92 @@
+import styled, { css } from "styled-components";
+import { theme } from "@/styles/theme";
+
+interface AlertProps {
+  title: string;
+  description: string;
+  alertType?: "delete" | "alert";
+  onClose?: () => void;
+}
+
+export function Alert({
+  title,
+  description,
+  alertType = "delete",
+  onClose,
+}: AlertProps) {
+  return (
+    <AlertWrapper>
+      <AlertTitle>{title}</AlertTitle>
+      <AlertDescription>{description}</AlertDescription>
+      <AlertButtonContainer>
+        <CancelButton $alertType={alertType} onClick={onClose}>
+          취소
+        </CancelButton>
+        <ConfirmButton onClick={onClose}>확인</ConfirmButton>
+      </AlertButtonContainer>
+    </AlertWrapper>
+  );
+}
+const AlertWrapper = styled.div`
+  position: absolute;
+  z-index: 1000;
+  top: 409px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${theme.colors.white};
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: ${theme.effects.effect1};
+  gap: 7px;
+`;
+
+const AlertTitle = styled.div`
+  font-size: ${theme.texts.h3.fontSize};
+  font-weight: ${theme.texts.h3.fontWeight};
+  line-height: ${theme.texts.h3.lineHeight};
+  color: ${theme.colors.gray1};
+`;
+
+const AlertDescription = styled.div`
+  font-size: ${theme.texts.body3.fontSize};
+  font-weight: ${theme.texts.body3.fontWeight};
+  line-height: ${theme.texts.body3.lineHeight};
+  color: ${theme.colors.gray2};
+  width: 245px;
+`;
+
+const AlertButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 12px;
+`;
+
+const CancelButton = styled.button<{ $alertType?: "alert" | "delete" }>`
+  background-color: ${theme.colors.gray5};
+  color: ${theme.colors.gray2};
+  padding: 6px 18px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  font-size: ${theme.texts.body2.fontSize};
+  font-weight: ${theme.texts.body2.fontWeight};
+  line-height: ${theme.texts.body2.lineHeight};
+
+  ${({ $alertType }) =>
+    $alertType === "alert" &&
+    css`
+      display: none;
+    `}
+`;
+
+const ConfirmButton = styled.button`
+  background-color: ${theme.colors.main};
+  color: ${theme.colors.white};
+  padding: 6px 18px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  font-size: ${theme.texts.body2.fontSize};
+  font-weight: ${theme.texts.body2.fontWeight};
+  line-height: ${theme.texts.body2.lineHeight};
+`;
