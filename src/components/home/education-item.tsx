da8a46265bb20aac9afe594f4a723a64bd403218
nyun-nye <2001_11_7@naver.com>
2025-10-25 19:42:29 +0900

    ✨: education-item 컴포넌트 생성

---
+++ src/components/home/education-item.tsx
@@ -0,0 +1,52 @@
+import { theme } from "@/styles/theme";
+import styled from "styled-components";
+import ArrowRightIcon from "@/assets/icons/ic_arrow-right";
+export function EducationItem({
+  title,
+  date,
+}: {
+  title: string;
+  date: string;
+}) {
+  return (
+    <EducationItemWrapper>
+      <EducationItemTitle>
+        <EducationItemTitleText>{title}</EducationItemTitleText>
+        <EducationItemDate>{date}</EducationItemDate>
+      </EducationItemTitle>
+      <ArrowRightIcon width={6} height={10} color={theme.colors.main} />
+    </EducationItemWrapper>
+  );
+}
+
+const EducationItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${theme.colors.sub4};
+  padding: 21px 18px;
+  border-radius: 20px;
+  border: 1px dashed ${theme.colors.main};
+`;
+
+const EducationItemTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+
+const EducationItemTitleText = styled.div`
+  font-size: ${theme.texts.body6.fontSize};
+  font-weight: ${theme.texts.body6.fontWeight};
+  line-height: ${theme.texts.body6.lineHeight};
+  color: ${theme.colors.gray1};
+`;
+
+const EducationItemDate = styled.div`
+  font-size: ${theme.texts.date.fontSize};
+  font-weight: ${theme.texts.date.fontWeight};
+  line-height: ${theme.texts.date.lineHeight};
+  color: ${theme.colors.gray3};
+`;
